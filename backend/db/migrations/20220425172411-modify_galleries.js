6530dd4f0ebcf5d7bf429fad41a9acae335c84ee
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-04-25 13:32:30 -0400

    added orderNumber column to galleries and images to allow custom sorting of galleries and images

---
+++ backend/db/migrations/20220425172411-modify_galleries.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return Promise.all([
+			queryInterface.addColumn('Galleries', 'orderNumber', {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				defaultValue: 0,
+			}),
+			queryInterface.addColumn('Images', 'orderNumber', {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				defaultValue: 0,
+			}),
+		]);
+	},
+
+	down: (queryInterface, Sequelize) => {
+		return Promise.all([
+			queryInterface.removeColumn('Galleries', 'orderNumber'),
+			queryInterface.removeColumn('Images', 'orderNumber'),
+		]);
+	},
+};
