90755ea8e32bd93d044fb6d7d513103db8927edb
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-06-01 21:59:24 -0400

    removed some unnecessary code

--- frontend/src/components/auth/LogoutButton.js
+++ frontend/src/components/auth/LogoutButton.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
-import { clearCampaignState } from '../../store/campaigns';
-import { clearCharacterState } from '../../store/characters';
-import { clearSectionState } from '../../store/sections';
-import { clearArticleState } from '../../store/articles';
 import './auth.css';
 import { useHistory } from 'react-router-dom';
 
 const LogoutButton = () => {
 	const history = useHistory();
 	const dispatch = useDispatch();
+
 	const onLogout = async (e) => {
-		await dispatch(clearCampaignState());
-		await dispatch(clearCharacterState());
-		await dispatch(clearSectionState());
-		await dispatch(clearArticleState());
 		await dispatch(logout());
 		history.push('/');
 	};
