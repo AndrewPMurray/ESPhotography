5274e63529634a44c1ff92427789f945e0523e9f
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-07-06 22:24:52 -0400

    small edit to safari polyfill.js

--- frontend/src/components/Gallery/safari-polyfill.js
+++ frontend/src/components/Gallery/safari-polyfill.js
@@ -1,5 +1,5 @@
 import { elementScrollIntoViewPolyfill } from 'seamless-scroll-polyfill';
 
-if (typeof window !== undefined && typeof document !== undefined) {
+if (typeof window !== 'undefined' && typeof document !== 'undefined') {
 	elementScrollIntoViewPolyfill();
 }
