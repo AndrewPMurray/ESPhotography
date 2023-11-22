65ce59cf4d1c9ff6ca3b5ff37d886c1504ea7876
AndrewPMurray <andrewpmurray1987@gmail.com>
2023-11-22 04:12:15 -0500

    testing next deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,10 +5,7 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination:
-					process.env.NODE_ENV !== 'production'
-						? 'http://localhost:5000/api/:path*'
-						: 'https://elmarschmittou.com:5000/api/:path*',
+				destination: 'http://localhost:5000/api/:path*',
 			},
 		];
 	},
