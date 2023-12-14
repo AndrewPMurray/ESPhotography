fd31dfabb449cb3ae7a8f188e20fdd493b90bff7
AndrewPMurray <AndrewPMurray1987@gmail.com>
2023-12-13 23:56:36 -0500

    update keywords metatag

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,24 +15,8 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
 	title: 'Elmar Schmittou Photography',
 	description: 'Professional photographs by Indianapolis based photographer, Elmar Schmittou.',
-	keywords: `
-				professional photographer,professional photography,Photography,
-				Photographer,Indianapolis Photographer,Photo,Photos,photoshoot,indianapolis photos,
-				fine art,art,photo art,prints,indy photographer,indy photos,Indianapolis Photographer,
-				Indiana Photographer,circle city photographer,Midwest photographer,portrait,portraits,
-				family,family portraits,family photographer,indianapolis family photographer,
-				pet photographer,photo studio,dog photographer,
-				indianapolis dog photographer,cat photographer,
-				indianapolis cat photographer,couple photography,couples photography,
-				indianapolis couples photography,elope photographs indiana,elopement photography,
-				senior high school portraits,high school portraits,school portraits,newborn photography,
-				newborn photographer,indianapolis newborn photographer,candid photos,candid photographs,
-				candid photography,street style photographs,street style photography,black white photography,
-				black white photographs,nature photography,photo headshot,photography commissions,Elmar,
-				elmar schmittou,best photographer,best indianapolis photographer,best indianapolis photography,
-				talented photographer,skilled photographer,skilled indiana photographer,
-				classy photographs indianapolis,classy photography indiana
-			  `,
+	keywords:
+		'professional photographer,professional photography,Photography,Photographer,Indianapolis Photographer,Photo,Photos,photoshoot,indianapolis photos,fine art,art,photo art,prints,indy photographer,indy photos,Indianapolis Photographer,Indiana Photographer,circle city photographer,Midwest photographer,portrait,portraits,family,family portraits,family photographer,indianapolis family photographer,pet photographer,photo studio,dog photographer,indianapolis dog photographer,cat photographer,indianapolis cat photographer,couple photography,couples photography,indianapolis couples photography,elope photographs indiana,elopement photography,senior high school portraits,high school portraits,school portraits,newborn photography,newborn photographer,indianapolis newborn photographer,candid photos,candid photographs,candid photography,street style photographs,street style photography,black white photography,black white photographs,nature photography,photo headshot,photography commissions,Elmar,elmar schmittou,best photographer,best indianapolis photographer,best indianapolis photography,talented photographer,skilled photographer,skilled indiana photographer,classy photographs indianapolis,classy photography indiana',
 	robots: 'index, follow',
 };
 
